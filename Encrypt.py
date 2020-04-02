8eb5e1fcdb4eabc6c9c867f1d704c3e0aa311ad9
Ben Mitchinson <mitchinson.dev@gmail.com>
2020-04-02 11:55:13 -0500

    Encrypt: user enter to progress each step

--- Encrypt.py
+++ Encrypt.py
@@ -1,52 +1,86 @@
 from FField import FField
 from keyExpansion import keyExpansion
-from sBox import sBox
-
-# from shiftRows import shiftRows
-# from mixColumns import mixColumns
+from sBox import sBox, subBytes
+from shiftRows import shiftRows
+from mixColumns import mixColumns
 from util import xorHexStr, addRoundKey
 
 
 class Encrypt:
-    def __init__(self, plainTxt, key, printStateChanges):
+    def __init__(self, plainTxt, key, printAllStateChanges):
         self.plainTxt = plainTxt
         self.key = key
         self.state = FField(plainTxt)
-        self.printStateChanges = printStateChanges
+        self.printAllStateChanges = printAllStateChanges
         self.currentRound = 0
-        self.roundKeys = []
         print(
             "***********************************************\n"
             + f"Encrypting: {plainTxt}\n"
             + f"Using Key : {key}\n"
-            + "***********************************************"
         )
         self.go()
 
     def go(self):
         roundKeys = keyExpansion(self.key)
-        if self.printStateChanges:
-            print(
-                f"round: {self.currentRound}\n"
-                + "***********************************************"
-            )
-            print("state:")
-            self.state.printStateAsGrid()
+        if self.printAllStateChanges:
+            self.printRound()
+            self.printChanges("initializing state")
 
         self.state = addRoundKey(self.state, roundKeys[self.currentRound])
+        if self.printAllStateChanges:
+            self.printChanges("adding round 0 addRoundKey")
         self.currentRound += 1
+        while self.currentRound < 10:
+            input(f"Press return to advance to round {self.currentRound}")
+            if self.printAllStateChanges:
+                self.printRound()
+                self.printChanges(f"state at the start of round {self.currentRound}")
 
-        if self.printStateChanges:
-            print("state:")
+            self.state = subBytes(self.state)
+            if self.printAllStateChanges:
+                self.printChanges("subbed bytes using sBox")
+
+            self.state = shiftRows(self.state)
+            if self.printAllStateChanges:
+                self.printChanges("shifted rows")
+
+            self.state = mixColumns(self.state)
+            if self.printAllStateChanges:
+                self.printChanges("shifted rows")
+
+            self.state = addRoundKey(self.state, roundKeys[self.currentRound])
+            if self.printAllStateChanges:
+                self.printChanges(f"adding round key {self.currentRound}")
+
+            self.currentRound += 1
+            # End of Core Loop (rounds 1-9)
+
+        input(f"Press return to advance to round {self.currentRound} (final round)")
+
+        self.state = subBytes(self.state)
+        if self.printAllStateChanges:
+            self.printChanges("final byteSub using sBox")
+        self.state = shiftRows(self.state)
+        if self.printAllStateChanges:
+            self.printChanges("final shift rows")
+        self.state = addRoundKey(self.state, roundKeys[self.currentRound])
+        if self.printAllStateChanges:
+            self.printChanges("final add round key")
+        if self.printAllStateChanges:
+            print(
+                "***********************************************\n"
+                + f"Final State / Cipher Output\n"
+                + "***********************************************"
+            )
             self.state.printStateAsGrid()
 
-        # while (self.currentRound < 10):
-        #   self.state = subBytes(self.state)
-        #   self.state = shiftRows(self.state)
-        #   self.state = mixColumns(self.state)
-        #   self.state = addRoundKey(self.state, self.roundKeys[self.currentRound])
-        #   self.currentRound+=1
+    def printChanges(self, reason):
+        print(f"state after {reason}:")
+        self.state.printStateAsGrid()
 
-        # self.state = subBytes(self.state)
-        # self.state = shiftRows(self.state)
-        # self.state = addRoundKey(self.state, self.roundKeys[self.currentRound])
+    def printRound(self):
+        print(
+            "***********************************************\n"
+            + f"round: {self.currentRound}\n"
+            + "***********************************************"
+        )
--- appendixB.py
+++ appendixB.py
@@ -5,7 +5,8 @@ from FField import FField
 def appendixB():
     demoPlainTxt = "3243f6a8885a308d313198a2e0370734"
     demoKey = "2b7e151628aed2a6abf7158809cf4f3c"
-    Encrypt(demoPlainTxt, demoKey, printStateChanges=True)
+    result = Encrypt(demoPlainTxt, demoKey, printAllStateChanges=True)
+    # TODO: Compair result
 
 
 appendixB()
