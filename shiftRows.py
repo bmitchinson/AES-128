ddbb2506d8075e93bebe9c50be61be6107cc40fc
Ben Mitchinson <mitchinson.dev@gmail.com>
2020-04-02 23:22:08 -0500

    fixed shift rows - broken sched

--- shiftRows.py
+++ shiftRows.py
@@ -9,5 +9,5 @@ def shiftRows(state):
 
 def invShiftRows(state):
     s = state.state
-    newStateStr = f"{s[0][0]}{s[1][1]}{s[2][2]}{s[3][3]}{s[0][3]}{s[1][0]}{s[2][1]}{s[3][2]}{s[0][2]}{s[1][3]}{s[2][0]}{s[3][1]}{s[0][1]}{s[1][2]}{s[2][3]}{s[3][0]}"
+    newStateStr = f"{s[0][0]}{s[1][3]}{s[2][2]}{s[3][1]}{s[0][1]}{s[1][0]}{s[2][3]}{s[3][2]}{s[0][2]}{s[1][1]}{s[2][0]}{s[3][3]}{s[0][3]}{s[1][2]}{s[2][1]}{s[3][0]}"
     return FField(newStateStr)
